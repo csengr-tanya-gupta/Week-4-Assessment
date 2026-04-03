2c39e178da94041f4ff2637e2c7a65c363a72dbb
Tanya Gupta <csengr.tanya.gupta@gmail.com>
2026-04-03 11:42:55 +0530

    week 4 assessment

---
+++ book-borrow/src/main/java/com/capgemini/model/repository/BookRepository.java
@@ -0,0 +1,11 @@
+package com.capgemini.model.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.capgemini.model.entity.Book;
+
+@Repository
+public interface BookRepository extends JpaRepository<Book, Integer>{
+
+}
