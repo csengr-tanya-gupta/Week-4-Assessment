9818e240a5fab25ff69f096e0dda35e2e57ec2b2
Tanya Gupta <csengr.tanya.gupta@gmail.com>
2026-04-03 11:46:00 +0530

    week 4 assessment

---
+++ online-food-order-management-system/src/main/java/online_food_order_management_system/OnlineFoodOrderManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package online_food_order_management_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineFoodOrderManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineFoodOrderManagementSystemApplication.class, args);
+	}
+
+}
